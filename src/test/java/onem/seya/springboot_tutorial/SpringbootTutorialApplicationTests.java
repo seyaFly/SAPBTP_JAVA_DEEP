82544fc154a476ceddbbd831559ece3777074192
chencai <liuhua@mychery.com>
2020-11-24 17:35:29 +0800

    adjust test case

--- src/test/java/onem/seya/springboot_tutorial/SpringbootTutorialApplicationTests.java
+++ src/test/java/onem/seya/springboot_tutorial/SpringbootTutorialApplicationTests.java
@@ -1,19 +1,15 @@
 package onem.seya.springboot_tutorial;
 
 import onem.seya.springboot_tutorial.controller.lesson01.WelcomeController;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(WelcomeController.class)
@@ -26,7 +22,7 @@ class SpringbootTutorialApplicationTests {
      *打印欢迎消息
      */
     @Test
-    void testSpring() throws Exception {
+    void testHelloWordController() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/api/v1/welcome")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
