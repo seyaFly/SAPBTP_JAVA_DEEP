83cbbf8ee6bc84b9dfcaf86dcc294a747d968b11
chencai <liuhua@mychery.com>
2020-11-25 11:40:57 +0800

    add swagger as api document

---
+++ src/main/java/onem/seya/springboot_tutorial/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package onem.seya.springboot_tutorial.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@ComponentScan(basePackages = SwaggerConfig.BASE_PACKAGE)
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    public static final String BASE_PACKAGE = "onem.seya.springboot_tutorial.controller";
+
+    public ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Spring Boot tutorial document api")
+                .description(
+                        "An API for keeping track of your apis.   Don't forget to take it for a spin by clicking on the **Try it out** button next to each operation!")
+                .license("")
+                .licenseUrl("")
+                .termsOfServiceUrl("")
+                .version("1.0.0")
+                .build();
+    }
+
+    @Bean
+    public Docket customImplementation(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+}
--- src/main/java/onem/seya/springboot_tutorial/controller/phase01/WelcomeController.java
+++ src/main/java/onem/seya/springboot_tutorial/controller/phase01/WelcomeController.java
@@ -1,18 +1,29 @@
 package onem.seya.springboot_tutorial.controller.phase01;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
-@RequestMapping(WelcomeController.path)
+@RequestMapping(path = WelcomeController.path, produces = { APPLICATION_JSON_VALUE })
+@RequestScope
+@Api(value = WelcomeController.path)
 public class WelcomeController {
     public static final String path = "/api/v1";
 
+    @ApiOperation(value = "welcome message", nickname = "printWelcomeMessage", notes = "", response = String.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "success print welcome message"),
+            @ApiResponse(code = 400, message = "invalid request body")})
     @GetMapping(value = "/welcome", produces = {APPLICATION_JSON_VALUE})
     public String printWelcomeMessage(){
-        return "welcome to start the spring boot project in SCP Cloud Foundry";
+        String welcomeMessage = "welcome to start the spring boot project in SCP Cloud Foundry";
+        return welcomeMessage;
     }
 }
